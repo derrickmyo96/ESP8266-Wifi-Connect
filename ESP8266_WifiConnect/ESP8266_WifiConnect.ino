80710de4d3b00a52612515973d43b24e6d878149
Derrick Myo <37631554+derrickmyo96@users.noreply.github.com>
2018-11-18 18:19:35 +0800

    Add files via upload

---
+++ ESP8266_WifiConnect/ESP8266_WifiConnect.ino
@@ -0,0 +1,40 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid="wanchun";
+const char* password="shahm59474";
+
+int ledPin = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin,LOW);
+
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Wifi connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  Serial.println();
+  Serial.print("Connecting");
+
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  digitalWrite(ledPin, HIGH);
+  Serial.println();
+
+  Serial.println("Wifi Connected Successfully!");
+  Serial.print("NODEMCU IP address: ");
+  Serial.println(WiFi.localIP());
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
